52045392bcd46e3091ba178d144181d887f5c9eb
tankazuki <34229874+tankazuki@users.noreply.github.com>
2018-07-20 21:57:54 +0900

    edit blog.views.html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.order_by('created_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
-    
+    return render(request, "blog/post_list.html", {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {'post': post})
